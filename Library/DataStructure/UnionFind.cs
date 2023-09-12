985841228931724b31eb50fa4baa8e65a65dfe17
osorosia <64416398+osorosia@users.noreply.github.com>
2023-09-13 03:19:40 +0900

    UnionFind

--- Library/DataStructure/UnionFind.cs
+++ Library/DataStructure/UnionFind.cs
@@ -4,28 +4,28 @@ namespace CompLib.DataStructure
 {
     public class UnionFind
     {
-        long[] parent;
-        long[] size;
-        long N;
+        long[] _parent;
+        long[] _size;
+        long _n;
 
         public UnionFind(long n)
         {
-            parent = new long[n];
-            size = new long[n];
-            N = n;
+            _parent = new long[n];
+            _size = new long[n];
+            _n = n;
             for (long i = 0; i < n; i++)
             {
-                size[i] = 1;
-                parent[i] = i;
+                _size[i] = 1;
+                _parent[i] = i;
             }
         }
 
         public long Find(long x)
         {
-            if (x == parent[x])
+            if (x == _parent[x])
                 return x;
-            parent[x] = Find(parent[x]);
-            return parent[x];
+            _parent[x] = Find(_parent[x]);
+            return _parent[x];
         }
 
         public bool Same(long x, long y)
@@ -39,17 +39,17 @@ namespace CompLib.DataStructure
             y = Find(y);
             if (x == y)
                 return false;
-            if (size[x] < size[y])
+            if (_size[x] < _size[y])
                 (x, y) = (y, x);
-            size[x] += size[y];
-            parent[y] = x;
+            _size[x] += _size[y];
+            _parent[y] = x;
             return true;
         }
 
         public List<long> Roots()
         {
             var st = new SortedSet<long>();
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < _n; i++)
                 st.Add(Find(i));
 
             var ls = new List<long>();
@@ -62,13 +62,15 @@ namespace CompLib.DataStructure
         public SortedDictionary<long, List<long>> Groups()
         {
             var mp = new SortedDictionary<long, List<long>>();
-            for (long i = 0; i < N; i++)
+
+            for (long i = 0; i < _n; i++)
             {
                 Find(i);
-                if (!mp.ContainsKey(parent[i]))
-                    mp.Add(parent[i], new List<long>());
-                mp[parent[i]].Add(i);
+                if (!mp.ContainsKey(_parent[i]))
+                    mp.Add(_parent[i], new List<long>());
+                mp[_parent[i]].Add(i);
             }
+
             return mp;
         }
     }
