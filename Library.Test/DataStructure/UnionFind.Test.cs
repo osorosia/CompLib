a26f7266db2538e8d524d249871447b40fea65f6
osorosia <64416398+osorosia@users.noreply.github.com>
2023-09-13 03:54:12 +0900

    UnionFind

--- Library.Test/DataStructure/UnionFind.Test.cs
+++ Library.Test/DataStructure/UnionFind.Test.cs
@@ -10,11 +10,15 @@ namespace CompLib.Test.DataStructure
         {
             var uf = new UnionFind(10);
 
-            uf.Merge(0, 1);
-            uf.Merge(0, 2);
-            uf.Merge(2, 3);
-            uf.Merge(4, 5);
+            // Merge
+            Assert.True(uf.Merge(0, 1));
+            Assert.True(uf.Merge(0, 2));
+            Assert.True(uf.Merge(2, 3));
+            Assert.True(uf.Merge(4, 5));
 
+            Assert.False(uf.Merge(0, 1));
+
+            // Same
             Assert.True(uf.Same(0, 1));
             Assert.True(uf.Same(0, 2));
             Assert.True(uf.Same(0, 3));
