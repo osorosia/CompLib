d1c883736db04b64bdcccc2bde5013ae85f50643
osorosia <64416398+osorosia@users.noreply.github.com>
2023-09-16 23:46:56 +0900

    UnionFind

--- Library.Test/DataStructure/UnionFind.Test.cs
+++ Library.Test/DataStructure/UnionFind.Test.cs
@@ -1,30 +1,29 @@
 using Xunit;
 using CompLib.DataStructure;
 
-namespace CompLib.Test.DataStructure
+namespace CompLib.Test.DataStructure;
+
+public class UnionFindTest
 {
-    public class UnionFindTest
+    [Fact]
+    public void UnionFind_Test()
     {
-        [Fact]
-        public void UnionFind_Test()
-        {
-            var uf = new UnionFind(10);
+        var uf = new UnionFind(10);
 
-            // Merge
-            Assert.True(uf.Merge(0, 1));
-            Assert.True(uf.Merge(0, 2));
-            Assert.True(uf.Merge(2, 3));
-            Assert.True(uf.Merge(4, 5));
+        // Merge
+        Assert.True(uf.Merge(0, 1));
+        Assert.True(uf.Merge(0, 2));
+        Assert.True(uf.Merge(2, 3));
+        Assert.True(uf.Merge(4, 5));
 
-            Assert.False(uf.Merge(0, 1));
+        Assert.False(uf.Merge(0, 1));
 
-            // IsSame
-            Assert.True(uf.IsSame(0, 1));
-            Assert.True(uf.IsSame(0, 2));
-            Assert.True(uf.IsSame(0, 3));
-            Assert.True(uf.IsSame(4, 5));
+        // IsSame
+        Assert.True(uf.IsSame(0, 1));
+        Assert.True(uf.IsSame(0, 2));
+        Assert.True(uf.IsSame(0, 3));
+        Assert.True(uf.IsSame(4, 5));
 
-            Assert.False(uf.IsSame(0, 4));
-        }
+        Assert.False(uf.IsSame(0, 4));
     }
 }
