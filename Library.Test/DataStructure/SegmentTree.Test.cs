8b39f14410af76c6295505c330b20dc1b792dda0
osorosia <64416398+osorosia@users.noreply.github.com>
2023-10-01 06:29:15 +0900

    SegTree

--- Library.Test/DataStructure/SegmentTree.Test.cs
+++ Library.Test/DataStructure/SegmentTree.Test.cs
@@ -19,6 +19,12 @@ public class SegmentTreeTest
             Assert.Equal(10, st.Query(0, 4));
             Assert.Equal(10, st.Query(0, 5));
 
+            Assert.Equal(10, st.Query(0, 5));
+            Assert.Equal(11, st.Query(1, 5));
+            Assert.Equal(12, st.Query(2, 5));
+            Assert.Equal(13, st.Query(3, 5));
+            Assert.Equal(14, st.Query(4, 5));
+
             Assert.Equal(11, st.Query(1, 2));
             Assert.Equal(12, st.Query(2, 3));
             Assert.Equal(13, st.Query(3, 4));
@@ -73,6 +79,36 @@ public class SegmentTreeTest
             Assert.Equal(11, st.Query(1, 2));
             Assert.Equal(12, st.Query(2, 3));
             Assert.Equal(13, st.Query(3, 4));
+        }
+    }
+
+    [Fact]
+    public void SegmentTree_RMQ_Test()
+    {
+        {
+            // 10 11 12 13 14
+            var st = new SegmentTree(SegmentTreeType.RMQ, new long[] { 10, 11, 12, 13, 14 });
+            Assert.Equal(
+                new long[] { 10, 11, 12, 13, 14 },
+                new[] { st[0], st[1], st[2], st[3], st[4] }
+            );
+
+            Assert.Equal(10, st.Query(0, 1));
+            Assert.Equal(11, st.Query(0, 2));
+            Assert.Equal(12, st.Query(0, 3));
+            Assert.Equal(13, st.Query(0, 4));
+            Assert.Equal(14, st.Query(0, 5));
+
+            Assert.Equal(14, st.Query(0, 5));
+            Assert.Equal(14, st.Query(1, 5));
+            Assert.Equal(14, st.Query(2, 5));
+            Assert.Equal(14, st.Query(3, 5));
+            Assert.Equal(14, st.Query(4, 5));
+
+            Assert.Equal(11, st.Query(1, 2));
+            Assert.Equal(12, st.Query(2, 3));
+            Assert.Equal(13, st.Query(3, 4));
+            Assert.Equal(14, st.Query(4, 5));
         }
     }
 }
