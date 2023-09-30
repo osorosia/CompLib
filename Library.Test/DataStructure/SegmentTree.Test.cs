1b31ff14b4e0bba07522c21247f1f7e30dd4b8d1
osorosia <64416398+osorosia@users.noreply.github.com>
2023-10-01 06:37:34 +0900

    tiny fix

--- Library.Test/DataStructure/SegmentTree.Test.cs
+++ Library.Test/DataStructure/SegmentTree.Test.cs
@@ -7,78 +7,78 @@ public class SegmentTreeTest
     {
         {
             // 10 11 12 13 14
-            var st = new SegmentTree(SegmentTreeType.RmQ, new long[] { 10, 11, 12, 13, 14 });
+            var tree = new SegmentTree(SegmentTreeType.RmQ, new long[] { 10, 11, 12, 13, 14 });
             Assert.Equal(
                 new long[] { 10, 11, 12, 13, 14 },
-                new[] { st[0], st[1], st[2], st[3], st[4] }
+                new[] { tree[0], tree[1], tree[2], tree[3], tree[4] }
             );
 
-            Assert.Equal(10, st.Query(0, 1));
-            Assert.Equal(10, st.Query(0, 2));
-            Assert.Equal(10, st.Query(0, 3));
-            Assert.Equal(10, st.Query(0, 4));
-            Assert.Equal(10, st.Query(0, 5));
+            Assert.Equal(10, tree.Query(0, 1));
+            Assert.Equal(10, tree.Query(0, 2));
+            Assert.Equal(10, tree.Query(0, 3));
+            Assert.Equal(10, tree.Query(0, 4));
+            Assert.Equal(10, tree.Query(0, 5));
 
-            Assert.Equal(10, st.Query(0, 5));
-            Assert.Equal(11, st.Query(1, 5));
-            Assert.Equal(12, st.Query(2, 5));
-            Assert.Equal(13, st.Query(3, 5));
-            Assert.Equal(14, st.Query(4, 5));
+            Assert.Equal(10, tree.Query(0, 5));
+            Assert.Equal(11, tree.Query(1, 5));
+            Assert.Equal(12, tree.Query(2, 5));
+            Assert.Equal(13, tree.Query(3, 5));
+            Assert.Equal(14, tree.Query(4, 5));
 
-            Assert.Equal(11, st.Query(1, 2));
-            Assert.Equal(12, st.Query(2, 3));
-            Assert.Equal(13, st.Query(3, 4));
-            Assert.Equal(14, st.Query(4, 5));
+            Assert.Equal(11, tree.Query(1, 2));
+            Assert.Equal(12, tree.Query(2, 3));
+            Assert.Equal(13, tree.Query(3, 4));
+            Assert.Equal(14, tree.Query(4, 5));
         }
 
         {
             // 10 11 12 13
-            var st = new SegmentTree(SegmentTreeType.RmQ, new long[] { 10, 11, 12, 13 });
+            var tree = new SegmentTree(SegmentTreeType.RmQ, new long[] { 10, 11, 12, 13 });
             Assert.Equal(
                 new long[] { 10, 11, 12, 13 },
-                new[] { st[0], st[1], st[2], st[3] }
+                new[] { tree[0], tree[1], tree[2], tree[3] }
             );
 
-            Assert.Equal(10, st.Query(0, 1));
-            Assert.Equal(10, st.Query(0, 2));
-            Assert.Equal(10, st.Query(0, 3));
-            Assert.Equal(10, st.Query(0, 4));
+            Assert.Equal(10, tree.Query(0, 1));
+            Assert.Equal(10, tree.Query(0, 2));
+            Assert.Equal(10, tree.Query(0, 3));
+            Assert.Equal(10, tree.Query(0, 4));
 
-            Assert.Equal(11, st.Query(1, 2));
-            Assert.Equal(12, st.Query(2, 3));
-            Assert.Equal(13, st.Query(3, 4));
+            Assert.Equal(11, tree.Query(1, 2));
+            Assert.Equal(12, tree.Query(2, 3));
+            Assert.Equal(13, tree.Query(3, 4));
 
             // 10 11 12 -1
-            st[3] = -1;
+            tree[3] = -1;
             Assert.Equal(
                 new long[] { 10, 11, 12, -1 },
-                new[] { st[0], st[1], st[2], st[3] }
+                new[] { tree[0], tree[1], tree[2], tree[3] }
             );
 
-            Assert.Equal(-1, st.Query(0, 4));
-            Assert.Equal(-1, st.Query(1, 4));
-            Assert.Equal(-1, st.Query(2, 4));
-            Assert.Equal(-1, st.Query(3, 4));
+            Assert.Equal(-1, tree.Query(0, 4));
+            Assert.Equal(-1, tree.Query(1, 4));
+            Assert.Equal(-1, tree.Query(2, 4));
+            Assert.Equal(-1, tree.Query(3, 4));
 
-            Assert.Equal(10, st.Query(0, 1));
-            Assert.Equal(10, st.Query(0, 2));
-            Assert.Equal(10, st.Query(0, 3));
+            Assert.Equal(10, tree.Query(0, 1));
+            Assert.Equal(10, tree.Query(0, 2));
+            Assert.Equal(10, tree.Query(0, 3));
 
             // 10 11 12 13
-            st[3] = 13;
+            tree[3] = 13;
             Assert.Equal(
                 new long[] { 10, 11, 12, 13 },
-                new[] { st[0], st[1], st[2], st[3] }
+                new[] { tree[0], tree[1], tree[2], tree[3] }
             );
 
-            Assert.Equal(10, st.Query(0, 1));
-            Assert.Equal(10, st.Query(0, 2));
-            Assert.Equal(10, st.Query(0, 3));
-            Assert.Equal(10, st.Query(0, 4));
+            Assert.Equal(10, tree.Query(0, 1));
+            Assert.Equal(10, tree.Query(0, 2));
+            Assert.Equal(10, tree.Query(0, 3));
+            Assert.Equal(10, tree.Query(0, 4));
 
-            Assert.Equal(11, st.Query(1, 2));
-            Assert.Equal(12, st.Query(2, 3));
-            Assert.Equal(13, st.Query(3, 4));
+            Assert.Equal(11, tree.Query(1, 2));
+            Assert.Equal(12, tree.Query(2, 3));
+            Assert.Equal(13, tree.Query(3, 4));
         }
     }
 
@@ -87,28 +87,28 @@ public class SegmentTreeTest
     {
         {
             // 10 11 12 13 14
-            var st = new SegmentTree(SegmentTreeType.RMQ, new long[] { 10, 11, 12, 13, 14 });
+            var tree = new SegmentTree(SegmentTreeType.RMQ, new long[] { 10, 11, 12, 13, 14 });
             Assert.Equal(
                 new long[] { 10, 11, 12, 13, 14 },
-                new[] { st[0], st[1], st[2], st[3], st[4] }
+                new[] { tree[0], tree[1], tree[2], tree[3], tree[4] }
             );
 
-            Assert.Equal(10, st.Query(0, 1));
-            Assert.Equal(11, st.Query(0, 2));
-            Assert.Equal(12, st.Query(0, 3));
-            Assert.Equal(13, st.Query(0, 4));
-            Assert.Equal(14, st.Query(0, 5));
+            Assert.Equal(10, tree.Query(0, 1));
+            Assert.Equal(11, tree.Query(0, 2));
+            Assert.Equal(12, tree.Query(0, 3));
+            Assert.Equal(13, tree.Query(0, 4));
+            Assert.Equal(14, tree.Query(0, 5));
 
-            Assert.Equal(14, st.Query(0, 5));
-            Assert.Equal(14, st.Query(1, 5));
-            Assert.Equal(14, st.Query(2, 5));
-            Assert.Equal(14, st.Query(3, 5));
-            Assert.Equal(14, st.Query(4, 5));
+            Assert.Equal(14, tree.Query(0, 5));
+            Assert.Equal(14, tree.Query(1, 5));
+            Assert.Equal(14, tree.Query(2, 5));
+            Assert.Equal(14, tree.Query(3, 5));
+            Assert.Equal(14, tree.Query(4, 5));
 
-            Assert.Equal(11, st.Query(1, 2));
-            Assert.Equal(12, st.Query(2, 3));
-            Assert.Equal(13, st.Query(3, 4));
-            Assert.Equal(14, st.Query(4, 5));
+            Assert.Equal(11, tree.Query(1, 2));
+            Assert.Equal(12, tree.Query(2, 3));
+            Assert.Equal(13, tree.Query(3, 4));
+            Assert.Equal(14, tree.Query(4, 5));
         }
     }
 }
