07603c2368dd2bced24fe968eeefc7feadc63f7e
osorosia <64416398+osorosia@users.noreply.github.com>
2023-10-02 02:27:51 +0900

    scanner

---
+++ Library/Util/Scanner.cs
@@ -0,0 +1,63 @@
+namespace CompLib.Util
+{
+    using System;
+    using System.Linq;
+
+    class Scanner
+    {
+        private string[] _line;
+        private int _index;
+        private const char Separator = ' ';
+
+        public Scanner()
+        {
+            _line = new string[0];
+            _index = 0;
+        }
+
+        public string Next()
+        {
+            if (_index >= _line.Length)
+            {
+                string s;
+                do
+                {
+                    s = Console.ReadLine()!;
+                } while (s.Length == 0);
+
+                _line = s.Split(Separator);
+                _index = 0;
+            }
+
+            return _line[_index++];
+        }
+
+        public string ReadLine()
+        {
+            _index = _line.Length;
+            return Console.ReadLine()!;
+        }
+
+        public int Int() => int.Parse(Next());
+        public long Long() => long.Parse(Next());
+        public double Double() => double.Parse(Next());
+        public decimal Decimal() => decimal.Parse(Next());
+        public char Char() => Next()[0];
+        public char[] CharArray() => Next().ToCharArray();
+        public string String() => Next();
+
+        public string[] Array()
+        {
+            string s = Console.ReadLine()!;
+            _line = s.Length == 0 ? new string[0] : s.Split(Separator);
+            _index = _line.Length;
+            return _line;
+        }
+
+        public int[] IntArray() => Array().Select(int.Parse).ToArray();
+        public long[] LongArray() => Array().Select(long.Parse).ToArray();
+        public double[] DoubleArray() => Array().Select(double.Parse).ToArray();
+        public decimal[] DecimalArray() => Array().Select(decimal.Parse).ToArray();
+        public string[] StringArray() => Array();
+    }
+}
