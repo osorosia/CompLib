9603bcb45535b1e0c9ca8e49d8bbf495f3cd218d
osorosia <64416398+osorosia@users.noreply.github.com>
2023-10-22 02:43:42 +0900

    BitFlag

--- Library/Util/BitFlag.cs
+++ Library/Util/BitFlag.cs
@@ -2,6 +2,11 @@ namespace CompLib.Util
 {
     public class BitFlag
     {
+        public static long New(int i)
+        {
+            return 1L << i;
+        }
+
         public static bool Has(long bitFlag, int i)
         {
             return (bitFlag & (1L << i)) != 0;
